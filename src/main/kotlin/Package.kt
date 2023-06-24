dcf3cdc78717f345f0489dcc55a0a28c500396ac
brian <liar88828@gmail.com>
2023-06-24 16:31:33 +0700

    39 Package dan Import

---
+++ src/main/kotlin/Package.kt
@@ -0,0 +1,8 @@
+//import  com.company.belajar.util.sayHello
+import  com.company.belajar.util.*
+
+fun main() {
+  sayHello("Eko")
+//  com.company.belajar.util.sayGoodBye("Kurniawan")
+  sayGoodBye("Kurniawan")
+}
