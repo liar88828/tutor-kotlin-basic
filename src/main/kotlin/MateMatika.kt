7ac4456b8ba0d626883f3e5a35dbe76fb8ad2ef4
brian <liar88828@gmail.com>
2023-06-24 10:51:15 +0700

    10 type data operasi matematika

---
+++ src/main/kotlin/MateMatika.kt
@@ -0,0 +1,35 @@
+fun main() {
+//biasa
+  var result1: Int = 10 + 3
+  println(result1)
+  var result2: Int = 10 - 3
+  println(result2)
+  var result3: Int = 10 * 3
+  println(result3)
+  var result4: Int = 10 / 3
+  println(result4)
+  var result5: Int = 10 % 3
+  println(result5)
+
+  var total: Int = 0
+  val barang1 = 100
+  total = total + barang1
+  val barang2 = 200
+  total += barang2
+  val barang3 = 300
+  total = total + barang3
+  println(total)
+
+  total++// total=total+1
+  total--// total=total+1
+  total++// total=total+1
+  println(total)
+
+  val suhu = -5
+  println(suhu)
+
+  var sehat = true
+  println(sehat)
+  sehat = false
+  println(sehat)
+}
